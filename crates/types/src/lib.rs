1e484a2b2564ee9adecb60caebb59b712b702371
Siavashmorad <s.moradi2099@gmail.com>
2026-09-11 21:56:33 +0330

    feat: define core NOVA types

---
+++ crates/types/src/lib.rs
@@ -0,0 +1,11 @@
+use serde::{Deserialize, Serialize};
+
+pub type Hash = [u8; 32];
+pub type Address = [u8; 20];
+pub type Amount = u128;
+
+#[derive(Clone, Debug, Default, Eq, PartialEq, Serialize, Deserialize)]
+pub struct Account {
+    pub balance: Amount,
+    pub nonce: u64,
+}
